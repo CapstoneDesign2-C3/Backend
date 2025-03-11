be53e89b2bd7b712345629cc42c9df9155868efc
Suhyeon7497 <nucc1186@gmail.com>
2025-03-11 22:01:09 +0900

    [setting] initial commit

---
+++ src/main/java/capstone/design/control_automation/ControlAutomationApplication.java
@@ -0,0 +1,13 @@
+package capstone.design.control_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capstone/design/control_automation/ControlAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.design.control_automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
