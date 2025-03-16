b3e4f90417a18028990a199db3cd75cf670149cb
Suhyeon7497 <nucc1186@gmail.com>
2025-03-16 16:03:04 +0900

    [feat] report 날짜 조회 - view

--- src/main/java/capstone/design/control_automation/controller/ReportController.java
+++ src/main/java/capstone/design/control_automation/controller/ReportController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ReportController {
@@ -12,7 +13,16 @@ public class ReportController {
     public String reports(Model model) {
         List<String> dates = List.of("2025-03-01", "2025-03-02", "2025-03-03", "2025-03-04", "2025-03-05");
         model.addAttribute("dates", dates);
+        String recentDate = dates.get(dates.size() - 1);
+        model.addAttribute("report", recentDate);
         return "report/reports";
     }
 
+    @GetMapping("/reports/{date}")
+    public String getReport(Model model, @PathVariable String date) {
+        List<String> dates = List.of("2025-03-01", "2025-03-02", "2025-03-03", "2025-03-04", "2025-03-05");
+        model.addAttribute("dates", dates);
+        model.addAttribute("report", date);
+        return "report/reports";
+    }
 }
