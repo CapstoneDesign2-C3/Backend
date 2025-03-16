1bccdb4b8b253e873cecf62eb2b239834ab662aa
Suhyeon7497 <nucc1186@gmail.com>
2025-03-16 14:33:19 +0900

    [feat] report sidebar 추가

--- src/main/java/capstone/design/control_automation/controller/ReportController.java
+++ src/main/java/capstone/design/control_automation/controller/ReportController.java
@@ -1,14 +1,18 @@
 package capstone.design.control_automation.controller;
 
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ReportController {
 
     @GetMapping("/reports")
-    public String reports() {
-        return "reports";
+    public String reports(Model model) {
+        List<String> dates = List.of("2025-03-01", "2025-03-02", "2025-03-03", "2025-03-04", "2025-03-05");
+        model.addAttribute("dates", dates);
+        return "report/reports";
     }
 
 }
