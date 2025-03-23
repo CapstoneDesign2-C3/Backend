2ad7ba81255e170c848af35d94f870eca2c931cd
Suhyeon7497 <nucc1186@gmail.com>
2025-03-23 22:20:11 +0900

    [setting] elasticSearch 접속 설정

---
+++ src/main/java/capstone/design/control_automation/config/ElasticSearchConfig.java
@@ -0,0 +1,29 @@
+package capstone.design.control_automation.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${spring.data.elasticsearch.url}")
+    private String url;
+
+    @Value("${spring.data.elasticsearch.username}")
+    private String username;
+
+    @Value("${spring.data.elasticsearch.password}")
+    private String password;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+            .connectedTo(url)
+            .withBasicAuth(username, password)
+            .build();
+    }
+}
