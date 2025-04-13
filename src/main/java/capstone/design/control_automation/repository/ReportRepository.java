44efd5110177609860a1d4d0b9e8c6f8d8326c6d
Suhyeon7497 <nucc1186@gmail.com>
2025-04-13 15:42:39 +0900

    [feat] Report 가져오는 로직

---
+++ src/main/java/capstone/design/control_automation/repository/ReportRepository.java
@@ -0,0 +1,10 @@
+package capstone.design.control_automation.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface ReportRepository {
+
+    List<LocalDateTime> getReportDateTimes();
+
+}
