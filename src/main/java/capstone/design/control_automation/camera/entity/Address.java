9cdd8962be738bed30680b1a7513625badf73498
2iedo <162278702+2iedo@users.noreply.github.com>
2025-05-05 21:56:59 +0900

    refactor : 경로 변경

--- src/main/java/capstone/design/control_automation/camera/entity/Address.java
+++ src/main/java/capstone/design/control_automation/camera/entity/Address.java
@@ -1,15 +1,17 @@
-package capstone.design.control_automation.entity;
+package capstone.design.control_automation.camera.entity;
 
 import static lombok.AccessLevel.PUBLIC;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @NoArgsConstructor
 @AllArgsConstructor(access = PUBLIC) // memoryVideoRepository 전용
+@Getter
 public class Address {
 
     @Column(name = "address1")
