bd4fa4fa804d6b71ea3ec5ad48f329fdd90e0a50
2iedo <162278702+2iedo@users.noreply.github.com>
2025-05-29 22:23:22 +0900

    feat : controller 추가

--- src/main/java/capstone/design/control_automation/event/controller/EventRestController.java
+++ src/main/java/capstone/design/control_automation/event/controller/EventRestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/event")
+@RequestMapping("api/v1/event")
 @RequiredArgsConstructor
 public class EventRestController {
     private final EventService eventService;
