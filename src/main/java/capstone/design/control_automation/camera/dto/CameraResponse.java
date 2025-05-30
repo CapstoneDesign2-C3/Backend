19efc5e862b771f3fb8cf574729a8030ae6d2a3c
2iedo <162278702+2iedo@users.noreply.github.com>
2025-05-30 23:38:43 +0900

    feat : 카메라 관리 페이지 추가

--- src/main/java/capstone/design/control_automation/camera/dto/CameraResponse.java
+++ src/main/java/capstone/design/control_automation/camera/dto/CameraResponse.java
@@ -2,5 +2,5 @@ package capstone.design.control_automation.camera.dto;
 
 import capstone.design.control_automation.camera.entity.CameraStatus;
 
-public record CameraResponse(Double latitude, Double longitude, String angle, String address1, String address2, CameraStatus status) {
+public record CameraResponse(Long id, Double latitude, Double longitude, String angle, String address1, String address2, CameraStatus status) {
 }
