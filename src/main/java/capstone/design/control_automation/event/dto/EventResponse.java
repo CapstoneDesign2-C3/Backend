568969d3d5210893f91942e96a9b1567f80222d7
2iedo <162278702+2iedo@users.noreply.github.com>
2025-05-30 23:53:10 +0900

    feat : event 관리 페이지 추가

--- src/main/java/capstone/design/control_automation/event/dto/EventResponse.java
+++ src/main/java/capstone/design/control_automation/event/dto/EventResponse.java
@@ -1,4 +1,4 @@
 package capstone.design.control_automation.event.dto;
 
-public record EventResponse(String status, String keyword) {
+public record EventResponse(Long id, String status, String keyword) {
 }
