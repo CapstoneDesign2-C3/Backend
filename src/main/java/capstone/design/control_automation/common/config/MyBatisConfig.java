8c33fcc7e57a5dc06ebfc411f27b89bf2a982e88
Suhyeon7497 <nucc1186@gmail.com>
2025-07-07 13:25:10 +0900

    [setting] mybatis 설정

---
+++ src/main/java/capstone/design/control_automation/common/config/MyBatisConfig.java
@@ -0,0 +1,31 @@
+package capstone.design.control_automation.common.config;
+
+import javax.sql.DataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@Configuration
+@MapperScan
+public class MyBatisConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean factory = new SqlSessionFactoryBean();
+        factory.setDataSource(dataSource);
+        factory.setMapperLocations(
+            new PathMatchingResourcePatternResolver().getResources("classpath:xml/mapper/*.xml")
+        );
+
+        return factory.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
