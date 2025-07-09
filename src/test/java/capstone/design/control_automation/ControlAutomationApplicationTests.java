39f6b4d4a6c7601d496d4af0e5a069e587872125
Suhyeon7497 <nucc1186@gmail.com>
2025-07-09 15:39:26 +0900

    feat: test 깨지는 거 해결

--- src/test/java/capstone/design/control_automation/ControlAutomationApplicationTests.java
+++ src/test/java/capstone/design/control_automation/ControlAutomationApplicationTests.java
@@ -2,8 +2,11 @@ package capstone.design.control_automation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:local.env")
 class ControlAutomationApplicationTests {
 
 	@Test
