49dbb14b6004cbd10a6ce13027db919040b38da8
Suhyeon7497 <nucc1186@gmail.com>
2025-07-22 10:22:54 +0900

    feat: feature 불러오는 부분 분리

--- src/main/java/capstone/design/control_automation/detected_object/controller/dto/DetectedObjectResponse.java
+++ src/main/java/capstone/design/control_automation/detected_object/controller/dto/DetectedObjectResponse.java
@@ -1,6 +1,5 @@
 package capstone.design.control_automation.detected_object.controller.dto;
 
-import capstone.design.control_automation.detected_object.repository.dto.DetectedObjectQueryResult;
 import capstone.design.control_automation.video.repository.dto.VideoQueryResult.Detail;
 
 public class DetectedObjectResponse {
@@ -12,14 +11,6 @@ public class DetectedObjectResponse {
         String feature
     ) {
 
-        public static Common of(Detail detail) {
-            return new Common(
-                detail.detectedObjectId(),
-                detail.categoryName(),
-                detail.cropImgUrl(),
-                detail.feature()
-            );
-        }
     }
 
     public record MobileObject(
