2c1a65e9da46fde4f0c998383a70a8178a216c94
Suhyeon7497 <nucc1186@gmail.com>
2025-07-22 09:24:24 +0900

    feat: eventresponse에 eventId 추가

--- src/test/java/capstone/design/control_automation/mapper/event/EventMapperTest.java
+++ src/test/java/capstone/design/control_automation/mapper/event/EventMapperTest.java
@@ -54,6 +54,7 @@ public class EventMapperTest {
             new Info(
                 24L,
                 "/thumbs/thumb4.jpg",
+                1L,
                 "a9778202-6320-43c7-b4a4-404d03513921",
                 "배회",
                 LocalDateTime.parse("2025-07-21 08:00:00", formatter),
@@ -62,6 +63,7 @@ public class EventMapperTest {
             new Info(
                 38L,
                 "/thumbs/thumb18.jpg",
+                19L,
                 "b57312ae-45d7-4b60-9e57-a8e47f7ac9ff",
                 "배회",
                 LocalDateTime.parse("2025-07-21 08:54:00", formatter),
