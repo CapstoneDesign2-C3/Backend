5470929023a3a9a6422ebc3fdef0378815db45e3
Suhyeon7497 <nucc1186@gmail.com>
2025-07-29 16:41:49 +0900

    setting: restClient 설정

--- src/main/java/capstone/design/control_automation/common/config/RestClientConfig.java
+++ src/main/java/capstone/design/control_automation/common/config/RestClientConfig.java
@@ -1,7 +1,18 @@
 package capstone.design.control_automation.common.config;
 
+import java.time.Duration;
+import org.apache.hc.client5.http.config.ConnectionConfig;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.core5.http.io.SocketConfig;
+import org.apache.hc.core5.util.TimeValue;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -14,7 +25,34 @@ public class RestClientConfig {
 
     @Bean
     public RestClient googleClient() {
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(20);
+        connectionManager.setDefaultMaxPerRoute(20);
+
+        ConnectionConfig connectionConfig = ConnectionConfig.custom()
+            .setConnectTimeout(Timeout.ofSeconds(5))
+            .build();
+        connectionManager.setDefaultConnectionConfig(connectionConfig);
+
+        SocketConfig socketConfig = SocketConfig.custom()
+            .setTcpNoDelay(true)
+            .setSoKeepAlive(true)
+            .build();
+        connectionManager.setDefaultSocketConfig(socketConfig);
+
+        RequestConfig requestConfig = RequestConfig.custom()
+            .setConnectionRequestTimeout(Timeout.ofSeconds(2))
+            .setResponseTimeout(Timeout.ofSeconds(10))
+            .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+            .setConnectionManager(connectionManager)
+            .setDefaultRequestConfig(requestConfig)
+            .evictIdleConnections(TimeValue.ofSeconds(10))
+            .build();
+
         return RestClient.builder()
+            .requestFactory(new HttpComponentsClientHttpRequestFactory(httpClient))
             .baseUrl("https://maps.googleapis.com")
             .build();
     }
