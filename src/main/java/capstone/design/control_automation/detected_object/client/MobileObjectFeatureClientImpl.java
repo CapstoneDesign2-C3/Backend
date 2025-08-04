96d894ce17e9d67ddf8f594af9beb7d06eac5537
2iedo <dn06023@naver.com>
2025-08-04 14:46:53 +0900

    feat : Qdrant에서 조회하는 메소드의 Impl 코드 구현

--- src/main/java/capstone/design/control_automation/detected_object/client/MobileObjectFeatureClientImpl.java
+++ src/main/java/capstone/design/control_automation/detected_object/client/MobileObjectFeatureClientImpl.java
@@ -1,12 +1,36 @@
 package capstone.design.control_automation.detected_object.client;
 
+import capstone.design.control_automation.common.exception.ErrorCode;
+import capstone.design.control_automation.common.exception.ErrorException;
+import io.qdrant.client.QdrantClient;
+import io.qdrant.client.grpc.Points;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import static io.qdrant.client.ConditionFactory.matchKeyword;
+import static io.qdrant.client.WithPayloadSelectorFactory.enable;
+
 @Component
+@RequiredArgsConstructor
 public class MobileObjectFeatureClientImpl implements MobileObjectFeatureClient {
+    private final QdrantClient qdrantClient;
 
     @Override
-    public String getFeatureByUuid(String uuid) {
-        return "feature";
+    public String getFeatureByUuid(String uuid){
+        try {
+            Points.ScoredPoint scoredPoint = qdrantClient.queryAsync(Points.QueryPoints.newBuilder()
+                    .setCollectionName("event") //TODO 실제 collection 이름으로 변경
+                    .setLimit(1)
+                    .setFilter(Points.Filter.newBuilder()
+                            .addMust(matchKeyword("uuid", uuid))
+                            .addMust(matchKeyword("type", "object")).build()) //TODO 실제 키워드로 변경
+                    .setWithPayload(enable(true))
+                    .build()).get().get(0);
+
+            return scoredPoint.getPayloadMap().get("feature").getStringValue();
+        }
+        catch (Exception e){
+            throw new ErrorException(ErrorCode.SEARCH_FAILED);
+        }
     }
 }
--- src/main/java/capstone/design/control_automation/event/client/EventSummaryClientImpl.java
+++ src/main/java/capstone/design/control_automation/event/client/EventSummaryClientImpl.java
@@ -1,12 +1,40 @@
 package capstone.design.control_automation.event.client;
 
+import capstone.design.control_automation.common.exception.ErrorCode;
+import capstone.design.control_automation.common.exception.ErrorException;
+import io.qdrant.client.QdrantClient;
+
+import static io.qdrant.client.ConditionFactory.matchKeyword;
+import static io.qdrant.client.grpc.Points.Filter;
+import static io.qdrant.client.WithPayloadSelectorFactory.enable;
+
+import io.qdrant.client.grpc.Points.ScoredPoint;
+import io.qdrant.client.grpc.Points.QueryPoints;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class EventSummaryClientImpl implements EventSummaryClient {
+    private final QdrantClient qdrantClient;
 
     @Override
-    public String getSummaryByUuid(String uuid) {
-        return "summary";
+    public String getSummaryByUuid(String uuid){
+        try{
+            ScoredPoint scoredPoint = qdrantClient.queryAsync(QueryPoints.newBuilder()
+                            .setCollectionName("event") //TODO 실제 collection 이름으로 변경
+                            .setLimit(1)
+                            .setFilter(Filter.newBuilder()
+                                    .addMust(matchKeyword("uuid",uuid))
+                                    .addMust(matchKeyword("type", "event")).build()) //TODO 실제 키워드로 변경
+                            .setWithPayload(enable(true))
+                        .build()).get().get(0);
+
+            return scoredPoint.getPayloadMap().get("summary").getStringValue();
+        }
+        catch (Exception e){
+            throw new ErrorException(ErrorCode.SEARCH_FAILED);
+        }
     }
 }
