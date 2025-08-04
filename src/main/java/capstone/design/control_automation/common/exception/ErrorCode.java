24f45fbaf7ce1b99882d63c4889b917681da9632
2iedo <dn06023@naver.com>
2025-08-04 14:46:24 +0900

    feat : Qdrant 에러 코드 추가

--- src/main/java/capstone/design/control_automation/common/exception/ErrorCode.java
+++ src/main/java/capstone/design/control_automation/common/exception/ErrorCode.java
@@ -20,8 +20,10 @@ public enum ErrorCode {
 
     //Category
     CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "카테고리를 찾을 수 없습니다."),
-    DETECTED_OBJECT_NOT_FOUND(HttpStatus.NOT_FOUND, "탐지 객체를 찾을 수 없습니다.");
+    DETECTED_OBJECT_NOT_FOUND(HttpStatus.NOT_FOUND, "탐지 객체를 찾을 수 없습니다."),
 
+    //Qdrant
+    SEARCH_FAILED(HttpStatus.CONFLICT, "검색에 실패하였습니다.");
     private final HttpStatus httpStatus;
     private final String message;
 
