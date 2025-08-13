0fc64652eea71fbb2d00fb584c2c05818c94678e
Suhyeon7497 <nucc1186@gmail.com>
2025-08-13 14:50:17 +0900

    refactor: service 예외처리를 client에서

--- src/main/java/capstone/design/control_automation/common/client/GoogleStaticMapApiClient.java
+++ src/main/java/capstone/design/control_automation/common/client/GoogleStaticMapApiClient.java
@@ -1,9 +1,12 @@
 package capstone.design.control_automation.common.client;
 
+import capstone.design.control_automation.common.exception.ExternalApiErrorCode;
+import capstone.design.control_automation.common.exception.ExternalApiException;
 import java.net.URI;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -11,17 +14,33 @@ import org.springframework.web.client.RestClient;
 @Component
 public class GoogleStaticMapApiClient {
 
+    private static final String STATIC_MAP_WARNING_HEADER = "X-Staticmap-API-Warning";
+
     private final RestClient googleClient;
 
     public GoogleStaticMapApiClient(@Qualifier("googleClient") RestClient googleClient) {
         this.googleClient = googleClient;
     }
 
-    public ResponseEntity<byte[]> exchange(URI uri) {
+    public byte[] exchange(URI uri) {
         return googleClient.get()
             .uri(uri)
-            .retrieve()
-            .toEntity(byte[].class);
+            .exchange((req, res) -> {
+                HttpStatus statusCode = (HttpStatus) res.getStatusCode();
+                if (statusCode.isSameCodeAs(HttpStatus.BAD_REQUEST)) {
+                    throw new ExternalApiException(ExternalApiErrorCode.GOOGLE_STATIC_MAP_INVALID_PARAM);
+                }
+                if (statusCode.isSameCodeAs(HttpStatus.FORBIDDEN)) {
+                    throw new ExternalApiException(ExternalApiErrorCode.GOOGLE_STATIC_MAP_INVALID_KEY);
+                }
+
+                HttpHeaders headers = res.getHeaders();
+                if (statusCode.is2xxSuccessful() && headers.containsKey(STATIC_MAP_WARNING_HEADER)) {
+                    log.warn("Google Static Map warning : {}", headers.get(STATIC_MAP_WARNING_HEADER));
+                }
+
+                return res.bodyTo(byte[].class);
+            });
     }
 
 }
--- src/main/java/capstone/design/control_automation/common/client/GoogleStaticMapService.java
+++ src/main/java/capstone/design/control_automation/common/client/GoogleStaticMapService.java
@@ -16,30 +16,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class GoogleStaticMapService {
 
-    private static final String STATIC_MAP_WARNING_HEADER = "X-Staticmap-API-Warning";
-
     private final GoogleStaticMapApiClient googleStaticMapApiClient;
     private final GoogleStaticMapUriBuilder uriBuilder;
     private final GoogleStaticMapProperties googleStaticMapProperties;
 
     public byte[] getStaticMap(MapRequest request) throws ExternalApiException {
         URI uri = uriBuilder.buildUri(request, googleStaticMapProperties.apiKey());
-
-        ResponseEntity<byte[]> response = googleStaticMapApiClient.exchange(uri);
-
-        HttpStatus statusCode = (HttpStatus) response.getStatusCode();
-        if (statusCode.isSameCodeAs(HttpStatus.BAD_REQUEST)) {
-            throw new ExternalApiException(ExternalApiErrorCode.GOOGLE_STATIC_MAP_INVALID_PARAM);
-        }
-        if (statusCode.isSameCodeAs(HttpStatus.FORBIDDEN)) {
-            throw new ExternalApiException(ExternalApiErrorCode.GOOGLE_STATIC_MAP_INVALID_KEY);
-        }
-
-        HttpHeaders headers = response.getHeaders();
-        if (statusCode.is2xxSuccessful() && headers.containsKey(STATIC_MAP_WARNING_HEADER)) {
-            log.warn("Google Static Map warning : {}", headers.get(STATIC_MAP_WARNING_HEADER));
-        }
-
-        return response.getBody();
+        return googleStaticMapApiClient.exchange(uri);
     }
 }
